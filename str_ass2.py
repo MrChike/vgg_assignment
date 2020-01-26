64fdf52f42f5b01aeabbfab397fcb5dc1e9c8c27
MrChike <chikeegonu@gmail.com>
2020-01-26 12:40:11 +0100

    Update and rename str_ass2 to str_ass2.py

---
+++ str_ass2.py
@@ -0,0 +1,20 @@
+sample_string = 'The quick Brown Fox'
+upper_str_receiver = ''
+lower_str_receiver = ''
+
+
+def program():
+    global upper_str_receiver
+    global lower_str_receiver
+
+    for letter in sample_string:
+        if letter.isupper() == True:
+            upper_str_receiver += letter
+        elif letter.islower() == True:
+            lower_str_receiver += letter
+
+
+program()
+
+print(f"No. of Upper case Characters: {len(upper_str_receiver)}")
+print(f"No. of Lower case Characters: {len(lower_str_receiver)}")
