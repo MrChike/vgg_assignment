d567322c904135a507712a8181612034cc6bcfff
MrChike <chikeegonu@gmail.com>
2020-01-26 13:50:16 +0100

    Update str_ass2.py

--- str_ass2.py
+++ str_ass2.py
@@ -1,4 +1,4 @@
-# 1. Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
+# 1A. Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 
 sample_string = 'The quick Brown Fox'
 upper_str_receiver = ''
@@ -19,3 +19,17 @@ program()
 
 print(f"No. of Upper case Characters: {len(upper_str_receiver)}")
 print(f"No. of Lower case Characters: {len(lower_str_receiver)}")
+
+
+
+# 1B. Write a Python function to find the Max of three numbers ------------------------------------------------
+
+def program(*args):
+    return max(args)
+
+program(1000, 2000, 3000)
+
+# Result --> 3000
+
+
+# 2. Write a Python function that takes a number as a parameter and check the number is prime or not.
