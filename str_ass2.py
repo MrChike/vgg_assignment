fb0f55fcf91157382f9764dbbfc598e8486e0097
MrChike <chikeegonu@gmail.com>
2020-02-01 17:57:46 +0100

    Update str_ass2.py

--- str_ass2.py
+++ str_ass2.py
@@ -1,25 +1,20 @@
 # 1A. Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 
-sample_string = 'The quick Brown Fox'
-upper_str_receiver = ''
-lower_str_receiver = ''
-
 def program():
-    global upper_str_receiver
-    global lower_str_receiver
-
+    sample_string = 'The quick Brown Fox'
+    upper_str_receiver = ''
+    lower_str_receiver = ''
+    
     for letter in sample_string:
         if letter.isupper() == True:
             upper_str_receiver += letter
         elif letter.islower() == True:
             lower_str_receiver += letter
 
-            
+    print(f"No. of Upper case Characters: {len(upper_str_receiver)}")
+    print(f"No. of Lower case Characters: {len(lower_str_receiver)}")
+
 program()
-
-print(f"No. of Upper case Characters: {len(upper_str_receiver)}")
-print(f"No. of Lower case Characters: {len(lower_str_receiver)}")
-
 
 
 # 1B. Write a Python function to find the Max of three numbers ------------------------------------------------
